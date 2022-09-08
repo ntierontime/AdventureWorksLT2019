905e316429d89e6b2916b27437d3549aa8cc86bd
David Zhao <david.zhao.blog@gmail.com>
2022-09-08 07:30:20 -0400

    MauiXApp WIP

---
+++ AdventureWorksLT2019/MauiXApp/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AdventureWorksLT2019.MauiXApp.Pages;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AdventureWorksLT2019/MauiXApp/ViewModels/SettingsVM.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+
+namespace AdventureWorksLT2019.MauiXApp.ViewModels
+{
+    public class SettingsVM: ObservableObject
+    {
+        private AppTheme m_CurrentAppTheme;
+        public AppTheme CurrentAppTheme
+        {
+            get => m_CurrentAppTheme;
+            set => SetProperty(ref m_CurrentAppTheme, value);
+        }
+
+        private string m_CurrentLanguage;
+        public string CurrentLanguage
+        {
+            get => m_CurrentLanguage;
+            set => SetProperty(ref m_CurrentLanguage, value);
+        }
+
+        public ICommand ChangeAppThemeCommand { private set; get; }
+        public ICommand ChangeLanguageCommand { private set; get; }
+
+        public SettingsVM()
+        {
+            ChangeAppThemeCommand = new Command<AppTheme>(
+                execute: (AppTheme arg) =>
+                {
+                    CurrentAppTheme = arg;
+                });
+
+            ChangeLanguageCommand = new Command<string>(
+                execute: (string arg) =>
+                {
+                    CurrentLanguage = arg;
+                });
+        }
+    }
+}
