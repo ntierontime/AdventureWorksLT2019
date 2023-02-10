24b6c4e12836bce6f6f8664c41d377f9c71823e4
David Zhao <david.g.zhao@ontario.ca>
2023-02-10 12:18:15 -0500

    Update 1.HelloWorld1.tsx
    to test i18n

--- AdventureWorksLT2019/ReactNativeTSAppExpo/src/1.HelloWorld1.tsx
+++ AdventureWorksLT2019/ReactNativeTSAppExpo/src/1.HelloWorld1.tsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useTranslation } from 'react-i18next';
 
 function HelloWorld1() {
-  return (
-    // https://reactnative.dev/docs/view
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#3498db",
-      }}
-    >
-      {/* https://reactnative.dev/docs/text */}
-      <Text
-        style={{
-          fontSize: 30,
-          fontWeight: "bold",
-          color: "#fff",
-          textTransform: "uppercase",
-        }}
-      >
-        Hello world!
-      </Text>
-    </View>
-  );
+    const { t, i18n } = useTranslation();
+    return (
+        // https://reactnative.dev/docs/view
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#3498db",
+            }}
+        >
+            {/* https://reactnative.dev/docs/text */}
+            <Text
+                style={{
+                    fontSize: 30,
+                    fontWeight: "bold",
+                    color: "#fff",
+                    textTransform: "uppercase",
+                }}
+            >
+                Hello world! {t('AdvancedSearch')}
+            </Text>
+        </View>
+    );
 }
 
 HelloWorld1.title = "HelloWorld1";
