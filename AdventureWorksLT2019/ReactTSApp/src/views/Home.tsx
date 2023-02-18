56d98dc00f81421413c7499052c124173191535c
David Zhao <david.zhao.blog@gmail.com>
2023-02-18 07:20:39 -0500

    Update Home.tsx

--- AdventureWorksLT2019/ReactTSApp/src/views/Home.tsx
+++ AdventureWorksLT2019/ReactTSApp/src/views/Home.tsx
@@ -1,5 +1,9 @@
+import { useTranslation } from "react-i18next";
+
 export default function Home() {
+    const { t } = useTranslation();
+    
     return (
-        <h1>Some Text</h1>
+        <h1>{t("Home")}</h1>
     );
 }
